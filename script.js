b19518dcdfe31926216ff9e52c7d3107949dbc91
William Chiu <william.chiu16@trinityschoolnyc.org>
2015-04-28 06:53:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(100);
 }
+
+window.onload = function() {
+  document.getElementById("defaultcanvas").width = 100%;
+  document.getElementById("defaultcanvas").height = 100%;
+}
