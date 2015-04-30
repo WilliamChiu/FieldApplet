cbcf6bf082e840218afb6002f14c023b9150b15c
William Chiu <william.chiu16@trinityschoolnyc.org>
2015-04-30 10:27:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function keyPressed() {
     if (key === ' ') {
         placeCharge();
     }
-    else if (value === '67') {
+    else if (keyCode === '67') {
         background(100);
         for (var considerCharges = 0; considerCharges < charges.length; considerCharges++) {
             newellipse(charges[considerCharges] % width, charges[considerCharges] / width, 8, 8);
